4de298e24c9e82e9279c580e88a726dc5b9d5503
Suhrob-SDK <suhrob3428@mail.ru>
2024-03-17 17:03:18 +0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <mcv-topbar/>
+
+    <router-view/>
+  </div>  
+</template>
+
+<script>
+import McvTopbar from '@/components/Topbar.vue'
+import { actionTypes } from '@/store/modules/auth';
+
+export default {
+  name: 'McvApp',
+  components: {
+    McvTopbar
+  },
+  mounted() {
+    this.$store.dispatch(actionTypes.getCurrentUser)
+  },
+  
+}
+</script>
+
+<style>
+
+</style>@/components/TopBar.vue
---
+++ src/router/index.js
@@ -0,0 +1,45 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+import HomeView from '@/views/HomeView.vue'
+import Register from '@/views/Register.vue'
+import Login from '@/views/Login.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/register',
+    name: 'register',
+    component: Register
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: Login
+  },
+  {
+    path: '/createArticle',
+    name: 'createArticle',
+    //component: CreateArticle
+  },
+  {
+    path: '/settings',
+    name: 'settings',
+    //component: Settings
+  },
+  {
+    path: '/userProfile',
+    name: 'userProfile',
+    //omponent: UserProfile
+  }
+  
+]
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes
+})
+
+export default router
